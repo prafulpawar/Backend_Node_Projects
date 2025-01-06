d0141dfa452b91e7f95dda9bf6e666a705500de3
prafulpawar <prafuldukhi@gmail.com>
2025-01-07 00:37:27 +0530

    "Done"

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-// const catMe=require('cat-me')
-// // console.log(catMe())
+const catMe=require('cat-me')
+// console.log(catMe())
 
 // const http = require('http')
 
@@ -18,17 +18,21 @@
 const express = require('express');
 
 const app = express();
+app.set('view engine','ejs');
+app.use((req,res,next)=>{
+     console.log("This Is Middleware");
+     // bass hum return karate hai next 
+     // build in 
+     // custom 
+     // 3part middleware
+     
+     next();
+})
 
 app.get('/',(req,res)=>{
-     res.send('Hello World')
-})
-app.get('/about',(req,res)=>{
-     res.send('About Page')
+    res.render('index')
 })
 
-app.get('/profile',(req,res)=>{
-    res.send('Profile Page')
-})
 app.listen(3000,()=>{
     console.log('Server is running on port 3000') // Server started successfully on port 3000
 })
