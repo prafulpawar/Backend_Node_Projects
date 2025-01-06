102644cf8faf448fdf0b3b113be16e71ef8f3466
prafulpawar <prafuldukhi@gmail.com>
2025-01-07 00:01:33 +0530

    "Done"

--- app.js
+++ app.js
@@ -22,3 +22,13 @@ const app = express();
 app.get('/',(req,res)=>{
      res.send('Hello World')
 })
+app.get('/about',(req,res)=>{
+     res.send('About Page')
+})
+
+app.get('/profile',(req,res)=>{
+    res.send('Profile Page')
+})
+app.listen(3000,()=>{
+    console.log('Server is running on port 3000') // Server started successfully on port 3000
+})
