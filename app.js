48c160f5b8bfb20774fb1852fed9a405a9b2615c
prafulpawar <prafuldukhi@gmail.com>
2025-01-06 23:33:30 +0530

    "Node"

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const http = require('http')
 
 const server=http.createServer((req,res)=>{
     res.end(catMe())
+    console.log(req.url)
 })
 server.listen(3000,()=>{
     console.log('Server is running on port 3000')  // Server started successfully on port 3000
