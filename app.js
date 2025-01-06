851cfc855edb1f01667199ac4bdd988193422c94
prafulpawar <prafuldukhi@gmail.com>
2025-01-06 23:28:37 +0530

    "Done"

--- app.js
+++ app.js
@@ -1,2 +1,11 @@
 const catMe=require('cat-me')
-console.log(catMe())
+// console.log(catMe())
+
+const http = require('http')
+
+const server=http.createServer((req,res)=>{
+    res.end(catMe())
+})
+server.listen(3000,()=>{
+    console.log('Server is running on port 3000')  // Server started successfully on port 3000
+})
