82cb3669ad49f0261c67c5d3e5460026df8d6aab
prafulpawar <prafuldukhi@gmail.com>
2025-01-09 12:32:04 +0530

    "Done"

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
-// const catMe=require('cat-me')
-// console.log(catMe())
+const catMe=require('cat-me')
+console.log(catMe())
 
-// const http = require('http')
+const http = require('http')
 
-// const server=http.createServer((req,res)=>{
-//      if(req.url == '/about'){
-//         res.end('aboutPage')
-//      }
-//      else {
-//          res.end(catMe())
-//      }
-// })
-// server.listen(3000,()=>{
-//     console.log('Server is running on port 3000')  // Server started successfully on port 3000
-// })
+const server=http.createServer((req,res)=>{
+     if(req.url == '/about'){
+        res.end('aboutPage')
+     }
+     else {
+         res.end(catMe())
+     }
+})
+server.listen(3000,()=>{
+    console.log('Server is running on port 3000')  // Server started successfully on port 3000
+})
 
 // const express = require('express');
 // const app = express();
