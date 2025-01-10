d3792b8130851a28e9e15de101a6faf49d699d86
prafulpawar <prafuldukhi@gmail.com>
2025-01-10 12:10:32 +0530

    'Rendering The Ejs'

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ const express = require('express');
 const app = express();
 const PORT = 3000 
 
+
+app.set('view engine','ejs')
 app.get('/',(req,res)=>{
-    res.send('Hello, World!!!')
+    res.render('index')
 })
+
+
 app.listen(PORT,(req,res)=>{
     console.log(`Server Running On Port ${PORT}`);
 })
-
