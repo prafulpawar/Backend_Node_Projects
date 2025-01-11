103422428b3457936033d482e480fe2e1040f033
prafulpawar <prafuldukhi@gmail.com>
2025-01-11 11:45:55 +0530

    "Done Rendering EJS"

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
 const express = require('express');
+const fs = require('fs')
+const PATH = require('path')
 const app = express();
 const PORT = 3000 
 
 
 app.set('view engine','ejs')
+
+app.use((req,res,next)=>{
+    console.log("World ")
+    next()
+})
+app.use(express.static(PATH.join(__dirname,"public")))
+
+console.log(express.static(PATH.join(__dirname,"public")))
+
+
+
 app.get('/',(req,res)=>{
+    res.render('index')
+})
+
+app.get('/contact',(req,res)=>{
     res.render('index')
 })
 
