69417723c006d0ece9d004a5d14bd97ce5a95c92
prafulpawar <prafuldukhi@gmail.com>
2025-01-11 12:32:48 +0530

    "Basics"

--- app.js
+++ app.js
@@ -6,8 +6,6 @@ const PORT = 3000
 
 app.set('view engine','ejs')
 
-
-
 app.use(express.static(path.join(__dirname,"public")))
 app.use(express.urlencoded({extended:true}))
 app.use(express.json());
