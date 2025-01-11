4b672f31704d83ead81cacb3c6977d1e98a79193
prafulpawar <prafuldukhi@gmail.com>
2025-01-11 12:29:23 +0530

    "Done"

--- app.js
+++ app.js
@@ -1,28 +1,28 @@
 const express = require('express');
 const fs = require('fs')
-const PATH = require('path')
+const path = require('path')
 const app = express();
 const PORT = 3000 
 
-
 app.set('view engine','ejs')
 
-app.use((req,res,next)=>{
-    console.log("World ")
-    next()
-})
-app.use(express.static(PATH.join(__dirname,"public")))
-
-console.log(express.static(PATH.join(__dirname,"public")))
 
 
+app.use(express.static(path.join(__dirname,"public")))
+app.use(express.urlencoded({extended:true}))
+app.use(express.json());
 
 app.get('/',(req,res)=>{
     res.render('index')
+    
 })
+app.get('/create', (req, res) => {
+    res.redirect('/');
+});
 
-app.get('/contact',(req,res)=>{
-    res.render('index')
+app.post('/create',(req,res)=>{
+    console.log(req.body)
+    res.send("abcd")
 })
 
 
