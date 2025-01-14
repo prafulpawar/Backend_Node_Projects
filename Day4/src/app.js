83e49d88236d4aaed37f1d729f8a5670ac0cc19c
prafulpawar <prafuldukhi@gmail.com>
2025-01-14 12:31:59 +0530

    "MVC Done"

--- Day4/src/app.js
+++ Day4/src/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const router = require('./routes/index.routes.js');
-
+const productRoutes = require('../src/routes/product.route.js')
 app.use('/',router)
-
+app.use('/products',productRoutes)
 module.exports = app;
