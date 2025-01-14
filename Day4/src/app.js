b47e95734b2e24cec7ddb6fa4b543376812ecf57
prafulpawar <prafuldukhi@gmail.com>
2025-01-14 12:04:59 +0530

    "Till_Controllers"

--- Day4/src/app.js
+++ Day4/src/app.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const app = express();
 const Controller = require('./controllers/index.controller.js')
-
+const usersController = require('./controllers/user.controller.js')
+const productController = require('./controllers/product.controller.js')
 app.get('/',Controller.indexController)
+// app.get('/about',Controller.aboutController)
 
+app.get('/users/profile',usersController.indexController)
+app.get('/users/products',productController.getProdfucts)
 module.exports = app;
--- Day4/src/controllers/index.controller.js
+++ Day4/src/controllers/index.controller.js
@@ -1,3 +1,8 @@
 module.exports.indexController = (req,res)=>{
     res.send('Hello How Are You!');
 }
+
+
+module.exports.aboutController = (req,res)=>{
+    res.send('Hello How Are You This Is About!')
+}
